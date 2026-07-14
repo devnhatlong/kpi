4c1ef5fc86f408ee7e6a88aadbe0a0f316f61ac9
Raito Nguyen <dev.nhatlong@gmail.com>
2026-07-14 22:32:55 +0700

    init server

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('port');
+  app.setGlobalPrefix('api');
 
   await app.listen(port);
 }
