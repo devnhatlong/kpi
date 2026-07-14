ff263eb963d4cb23cc0681b8db8e1c02447b4d06
Raito Nguyen <dev.nhatlong@gmail.com>
2026-07-14 21:56:25 +0700

    init server

--- server/src/modules/users/schemas/user.schema.ts
+++ server/src/modules/users/schemas/user.schema.ts
@@ -1,1 +1,16 @@
-export class User {}
+
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+    @Prop()
+    name!: string;
+
+    @Prop()
+    age!: number;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
